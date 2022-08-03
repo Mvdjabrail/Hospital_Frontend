1104330bdd0d8e37a714d9633bef17337892a5b7
Djabrail Magamadov <magamadov.ru095@gmail.com>
2022-08-03 10:51:02 +0300

    Готово к работе

---
+++ src/components/Content/SigninInContent/SigninIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SigninIn = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SigninIn;
--- src/pages/SigninIn.jsx
+++ src/pages/SigninIn.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const SigninIn = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return <div></div>;
 };
 
 export default SigninIn;
