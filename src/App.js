7a77172a1807c0580d4fd536b1d71bf7b5ae5b14
Djabrail Magamadov <magamadov.ru095@gmail.com>
2022-08-03 11:10:24 +0300

    Теперь точно готово

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
-    <div className="APP">
       <BrowserRouter>
         <Routes>
           <Route path="/*" element={<Layout />}></Route>
         </Routes>
       </BrowserRouter>
-    </div>
   );
 }
 
