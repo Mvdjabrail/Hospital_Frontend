949824dc18f3d20d45b6c04374e372e73b76516c
AgReSSoR <samahkinets@gmail.com>
2022-08-03 11:20:20 +0300

    создал вайл пейдж и папку для контакт компонента

---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Contacts = () => {
+    return (
+        <Container>
+
+            <Contacts/>
+            
+        </Container>
+    );
+};
+
+export default Contacts;
