6066ee39902c655196b8509c3316cd271cb4a333
AgReSSoR <samahkinets@gmail.com>
2022-08-03 16:30:00 +0300

    2

---
+++ src/pages/ContactsPages.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Contacts from "../components/Content/ContactsContent/Contacts";
+
+const ContactsPages = () => {
+  return (
+    <Container>
+      <Contacts />
+     
+    </Container>
+  );
+};
+
+export default ContactsPages;
