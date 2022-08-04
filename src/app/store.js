ad8d80f76a2e40b5351e9bbf87171b96fc938720
JuniorTIM <musa.aliev06@yandex.ru>
2022-08-04 18:00:36 +0300

    аптека в процессе

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import usersReducer from '../features/users/userSlice'
+import usersReducer from "../features/users/userSlice";
+import drugsReducer from "../features/drugs/drugsSlice";
+import categoriesReducer from "../features/category/categorySlice";
 
 export const store = configureStore({
-    reducer: {
-        usersReducer: usersReducer,
-    }
+  reducer: {
+    usersReducer: usersReducer,
+    drugsReducer: drugsReducer,
+    categoriesReducer: categoriesReducer,
+  },
 });
